4ab79b05d6a4a3d2446f82b882eb81915c0802ed
jy <977996067@qq.com>
2022-08-22 01:46:12 +0800

    :memo: Add CompletableFuture.adoc

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
+NOTE_DIR=$(pwd)
 TMP_DIR=/tmp/blog/
 ORIGIN_ADDRESS=git@github.com:jjyyjjyy/tech.git
 ORIGIN_BRANCH=gh-pages
-cd notes || return
-mvn clean asciidoctor:process-asciidoc
+
 rm -rf ${TMP_DIR} && mkdir -p ${TMP_DIR}
-cp -r target/generated-docs/** ${TMP_DIR}
 cd ${TMP_DIR} || return
 git init
 git remote add origin ${ORIGIN_ADDRESS}
+git fetch -p
+git checkout ${ORIGIN_BRANCH}
+
+cd ${NOTE_DIR}/notes || return
+mvn clean asciidoctor:process-asciidoc
+cp -r target/generated-docs/** ${TMP_DIR}
+
+cd ${TMP_DIR} || return
 git add .
 git commit -m ":rocket: Update"
-git checkout -b ${ORIGIN_BRANCH}
-git checkout -b tmp
-git branch -D ${ORIGIN_BRANCH}
-git push origin --delete ${ORIGIN_BRANCH}
-git checkout -b ${ORIGIN_BRANCH}
 git push --set-upstream origin ${ORIGIN_BRANCH}
 cd - || return
 echo "${ORIGIN_BRANCH} sync successfully!"
